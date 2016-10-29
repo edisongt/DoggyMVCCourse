0b661a23a3c171bfd23f7b711e9a84c81da415f1
Will <doggy.huang@gmail.com>
2016-10-29 11:18:29 +0800

    復原屬性路由設定

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -15,7 +15,7 @@ namespace MVC5Course.Controllers
         ProductRepository repo = RepositoryHelper.GetProductRepository();
 
         // GET: Products
-        [Route("products/list")]
+        //[Route("products/list")]
         public ActionResult Index()
         {
             //var repo = new ProductRepository();
@@ -68,7 +68,7 @@ namespace MVC5Course.Controllers
         }
 
         // GET: Products/Edit/5
-        [Route("prod/edit/{id}")]
+        //[Route("prod/edit/{id}")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
