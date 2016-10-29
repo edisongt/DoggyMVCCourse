3f68cb5c49045e637f15eae57c561041d779ee5d
Will <doggy.huang@gmail.com>
2016-10-29 15:21:08 +0800

    43 練習複雜模型繫結並可一次傳入多筆複雜模型資料繫結到陣列裡

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class MBController : Controller
+    public class MBController : BaseController
     {
         public ActionResult Index()
         {
@@ -44,6 +44,39 @@ namespace MVC5Course.Controllers
 
         public ActionResult MyFormResult()
         {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            var data = db.Product.OrderBy(p => p.ProductId).Take(10);
+            return View(data);
+        }
+
+        public ActionResult BatchUpdate(ProductBatchUpdateViewModel[] items)
+        {
+            /*
+             * 預設輸出的欄位名稱格式：item.ProductId
+             * 要改成以下欄位格式：
+             * items[0].ProductId
+             * items[1].ProductId
+             */
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var product = db.Product.Find(item.ProductId);
+                    product.ProductName = item.ProductName;
+                    product.Active = item.Active;
+                    product.Stock = item.Stock;
+                    product.Price = item.Price;
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("ProductList");
+            }
+
             return View();
         }
 
